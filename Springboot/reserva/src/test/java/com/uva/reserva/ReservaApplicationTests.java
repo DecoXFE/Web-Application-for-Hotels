3bcb041f46ce12531892a0dea86cb6093168d0ea
izajime <izanmario8@gmail.com>
2024-10-14 19:35:22 +0200

    Initial commit SpringBoot

---
+++ Springboot/reserva/src/test/java/com/uva/reserva/ReservaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uva.reserva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
