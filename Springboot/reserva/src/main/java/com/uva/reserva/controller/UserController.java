bc1bec2b47b0190fecc1abf8841dddf92c6e6816
izajime <izanmario8@gmail.com>
2024-10-17 13:33:55 +0200

    deleteUser creado

--- Springboot/reserva/src/main/java/com/uva/reserva/controller/UserController.java
+++ Springboot/reserva/src/main/java/com/uva/reserva/controller/UserController.java
@@ -12,6 +12,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,5 +69,9 @@ public class UserController {
         }       
     }
 
-    
+    // Método para eliminar un usuario
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Integer id) {
+        repository.deleteById(id);
+    }
 }
